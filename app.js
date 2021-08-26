a3c114845b790e1abc7b65cb64aa5b985ee63203
lianming <15127004511@qq.com>
2021-08-26 18:13:20 +0800

    add

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-import * as lmgl from './src/lmgl.js'
+import lmgl from './src/lmgl.js'
 
-console.error(lmgl.version);
+// console.error(lmgl.version);
 
 const vertexShader = `
 void main(){
@@ -13,23 +13,14 @@ const fragmentShader = `
 	  gl_FragColor = vec4(1.,0.,0.,1.);
 	}
 	`
+
 window.onload = () => {
-  const canvas = document.getElementById("c")
-  const gl = canvas.getContext("webgl");
-  const program = lmgl.createProgram(gl, {
-    vertexShader: vertexShader,
-    fragmentShader: fragmentShader
-  });
-  gl.useProgram(program);
 
-  gl.clearColor(0, 0, 0, 1.0);
-  gl.clear(gl.COLOR_BUFFER_BIT);
-  gl.drawArrays(gl.POINTS, 0, 1);
-
+  let app = new lmgl()
+  app.initRender("c",window.innerWidth, window.innerHeight)
 
   window.lm = {
     lmgl,
-    gl
   }
 
 }
