f50b0542033d757e6c2ff97e004590cef0aa0625
lianming <you@example.com>
2021-11-18 20:13:14 +0800

    add

--- src/shaderLib/depth.js
+++ src/shaderLib/depth.js
@@ -22,11 +22,19 @@ export function getMaterial() {
 
   const fragmentShader = `
       precision mediump float;
-      ${ common }
-      ${ depth }
+      vec4 pack(float depth) {
+        // 使用rgba 4字节共32位来存储z值,1个字节精度为1/256
+        const vec4 bitShift = vec4(1.0, 256.0, 256.0 * 256.0, 256.0 * 256.0 * 256.0);
+        const vec4 bitMask = vec4(1.0 / 256.0, 1.0 / 256.0, 1.0 / 256.0, 0.0);
+        // gl_FragCoord:片元的坐标,fract():返回数值的小数部分
+        vec4 rgbaDepth = fract(depth * bitShift); //计算每个点的z值
+        rgbaDepth -= rgbaDepth.gbaa * bitMask; // Cut off the value which do not fit in 8 bits
+        return rgbaDepth;
+      }
 
       void main() {
-        gl_FragColor = vec4(1.,0.,0.,1.);//pack(gl_FragCoord.z);
+        gl_FragColor = pack(gl_FragCoord.z);
+        //  gl_FragColor = vec4(vec3(gl_FragCoord.z), 1.);
       }
       `
   return { vertexShader, fragmentShader }
