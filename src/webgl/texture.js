5f79bd32780bac74898a4c70adcb132444fbbbc4
lianming <15127004511@qq.com>
2021-12-30 18:50:33 +0800

    add

--- src/webgl/texture.js
+++ src/webgl/texture.js
@@ -35,6 +35,20 @@ export function setTextureImage(gl, img, texture) {
    // gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
 }
 
+function pbr(params) {
+  const cubemapTargets = getCubemapTargets(gl);
+
+  for (let j = 0; j < 6; j++) {
+    gl.texImage2D(cubemapTargets[j], 0, gl.RGBA16F, gl.RGBA, gl.HALF_FLOAT, images[j]);
+    gl.texParameteri(gl.TEXTURE_CUBE_MAP, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+    gl.texParameteri(gl.TEXTURE_CUBE_MAP, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+    gl.texParameterf(gl.TEXTURE_CUBE_MAP, gl.TEXTURE_MIN_FILTER, gl.LINEAR_MIPMAP_LINEAR);
+    gl.texParameterf(gl.TEXTURE_CUBE_MAP, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+  }
+  gl.generateMipmap(gl.TEXTURE_CUBE_MAP);
+  gl.bindTexture(gl.TEXTURE_CUBE_MAP, null);
+}
+
 // 设置cube纹理的图片
 export function setCubeTextureImage(gl, images, texture) {
    const cubemapTargets = getCubemapTargets(gl);
