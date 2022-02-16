faa52f17cf33ca9aea913df8f837f62addc3aa26
lianming <15127004511@qq.com>
2022-02-16 18:43:50 +0800

    add

--- ts-lmgl2-end/public/app.ts
+++ ts-lmgl2-end/public/app.ts
@@ -20,10 +20,14 @@ class App {
     });
 
     const app = new EasyCG.Application(engine);
+
     const boxModel = EasyCG.boxBuilder();
     const geometry = new EasyCG.Geometry(engine, boxModel);
+    const material = new EasyCG.Material();
+    const mesh = new EasyCG.Mesh(geometry, material)
 
-    console.error(geometry);
+    const camera = new EasyCG.Camera()
+    console.error(mesh);
 
     // var scene = BABYLON.creator(BABYLON.Scene, engine);
     app.runRenderLoop(() => {
@@ -31,65 +35,11 @@ class App {
     });
 
     window.addEventListener("resize", () => app.resizeCanvas());
-
     this.app = app;
-    // const scene = new lmgl.Scene(engine);
-
-    // const camera = new lmgl.TargetCamera("Camera", new lmgl.Vector3(0, 0, 10), scene);
-    // camera.target = new lmgl.Vector3(0, 0, 0);
-
-    // lmgl.Effect.ShadersStore["customVertexShader"] = `
-    // precision highp float;
-    // layout(std140, column_major) uniform;
-    //   uniform mat4 world;
-    //   in vec3 position;
-    //   out vec3 vPositionW;
-    //   out vec3 vNormalW;
-
-    //   uniform Scene {
-    //     mat4 viewProjection;
-    //     mat4 view;
-    //   };
-
-    //   void main(void) {
-    //     vec3 positionUpdated = position;
-    //     mat4 finalWorld = world;
-    //     vec4 worldPos = finalWorld*vec4(positionUpdated, 1.0);
-    //     gl_Position = viewProjection*worldPos;
-    //   }`;
-
-    // lmgl.Effect.ShadersStore["customFragmentShader"] = `
-    // precision highp float;
-    //   out vec4 glFragColor;
-    //   void main() {
-    //     glFragColor = vec4(1.,0.,0.,1.);
-    //   }
-    // `;
-    // const mat = new lmgl.ShaderMaterial("s", scene, "custom", {
-    //   uniforms: ["world", "worldView", "worldViewProjection", "view", "projection", "viewProjection"],
-    //   uniformBuffers: ["Scene"],
-    // });
-
-    // const sphereGeometryData = lmgl.getSphereGeometryBuilder();
-    // this.sphereMesh = new lmgl.Mesh("sphere", scene, sphereGeometryData);
-    // this.sphereMesh.material = mat;
-    // // console.error(mat);
-
-    // // var sphere0 = BABYLON.MeshBuilder.CreateSphere("sphere0", {}, scene);
-    // //  console.error(sphereGeometry);
-    // // this.createScene(this.engine, this.canvas);
-    // // this.engine.engineRender.runRenderLoop(() => {
-    // //   this.scene.sceneRender.render();
-    // // });
-
-    // engine.engineRender.runRenderLoop(() => {
-    //   scene.sceneRender.render();
-    // });
   }
 }
 
 window.onload = () => {
-  console.error(EasyCG);
   const app = new App();
   (window as any).lm = app;
 };
--- ts-lmgl2-end/src/index.ts
+++ ts-lmgl2-end/src/index.ts
@@ -2,3 +2,4 @@ export * from "./engines/engine";
 export * from "./application";
 export * from "./misc/logger";
 export * from "./meshes/index";
+export * from "./camera/camera"
