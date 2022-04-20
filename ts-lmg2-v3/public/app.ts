0d47772fbdff7b54d9b4d2f1be0478f268c848a7
lianming <15127004511@qq.com>
2022-04-20 14:12:54 +0800

    add

--- ts-lmg2-v3/public/app.ts
+++ ts-lmg2-v3/public/app.ts
@@ -12,6 +12,10 @@ let canvas: any;
 let engine: lmgl.Engine;
 let scene: lmgl.Scene;
 
+let stats = new (window as any).Stats();
+stats.showPanel(0); // 0: fps, 1: ms, 2: mb, 3+: custom
+document.body.appendChild(stats.dom);
+
 window.onload = () => {
     canvas = document.getElementById("renderCanvas");
     engine = new lmgl.Engine(canvas);
@@ -20,6 +24,10 @@ window.onload = () => {
     (window as any).app = app;
 
     run(engine, scene, app);
+
+    app.addUpdate("stats", () => {
+        stats.update();
+    });
 
     app.loop();
 };
